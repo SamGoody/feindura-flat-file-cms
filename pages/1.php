52df507f7b6aaa0c1845fe441aa32f4b8f09d8e7
SamGoody <siteroller@gmail.com>
2010-12-19 13:49:18 +0200

    Some changes to a page - should not really be a part of the repo.
    Signed-off-by: SamGoody <siteroller@gmail.com>

--- pages/1.php
+++ pages/1.php
@@ -21,22 +21,27 @@ $pageContent['styleFile'] =          '';
 $pageContent['styleId'] =            '';
 $pageContent['styleClass'] =         '';
 
-$pageContent['log_visitorcount'] =   '6';
+$pageContent['log_visitorcount'] =   '10';
 $pageContent['log_visitTime_min'] =  'a:10:{i:0;i:1062;i:1;i:278;i:2;i:263;i:3;i:212;i:4;i:161;i:5;i:116;i:6;i:88;i:7;i:76;i:8;i:31;i:9;i:15;}';
 $pageContent['log_visitTime_max'] =  'a:4:{i:0;i:73944;i:1;i:348;i:2;i:19;i:3;i:7;}';
 $pageContent['log_firstVisit'] =     '1290977875';
-$pageContent['log_lastVisit'] =      '1291801914';
+$pageContent['log_lastVisit'] =      '1292744401';
 $pageContent['log_searchwords'] =    '';
 
 $pageContent['content'] = 
-'<p>
-	&nbsp;</p>
-<p>
-	This is the welcome page.</p>
-<p>
-	lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;</p>
-<p>
-	lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;</p>
+'
+<p><span class="Apple-style-span" style="line-height: normal;">This is the welcome page.</span><br />
+
+<span class="Apple-style-span" style="line-height: normal;"><br />
+
+lorem ipsum dolor sit amet&nbsp;lorem ipsum&nbsp;</span></p>
+<hr id="false">
+<span class="Apple-style-span">sit amet&nbsp;</span><s><span class="Apple-style-span">lorem </span></s><span class="Apple-style-span">ipsum</span><blockquote> <span class="Apple-style-span">dolor </span>sit amet&nbsp;</blockquote> 
+<p></p>
+<p style="text-align: justify;"></p>lorem ipsum&nbsp; 
+<hr id="false">
+<div><blockquote>sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum&nbsp;dolor sit amet&nbsp;lorem</blockquote>ipsum&nbsp;dolor&nbsp;sit amet lor&nbsp;em ipsum dolor sit amet&nbsp;lorem ipsum dolor sit&nbsp;lorem ipsum&nbsp;dolor&nbsp;sit amet&nbsp;lorem&nbsp;ipsum&nbsp;dolor sit amet&nbsp;lorem ipsum dolor sit affmet&nbsp;lorem ipsum&nbsp;dolor sit am&nbsp;et&nbsp;lorem&nbsp;ipsum dolor&nbsp;sit amet&nbsp;lorem ipsum&nbsp;dolor&nbsp;sit&nbsp;a&nbsp;met&nbsp;</div>
+<p></p>
 ';
 
 return $pageContent;
